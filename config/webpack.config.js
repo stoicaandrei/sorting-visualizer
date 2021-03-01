ed84481afc7568396b5b14ab1058ccabfcaaa3d0
Andrei Stoica <andrei00stoica@gmail.com>
2021-03-02 01:47:13 +0200

    Make folders packages

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -313,7 +313,7 @@ module.exports = function (webpackEnv) {
       // We placed these paths second because we want `node_modules` to "win"
       // if there are any conflicts. This matches Node resolution mechanism.
       // https://github.com/facebook/create-react-app/issues/253
-      modules: ['node_modules', paths.appNodeModules].concat(
+      modules: ['node_modules', paths.appNodeModules, paths.appSrc].concat(
         modules.additionalModulePaths || []
       ),
       // These are the reasonable defaults supported by the Node ecosystem.
