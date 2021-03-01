bdf94ff9d02c554a62a2282bacb05cc95346fd92
Andrei Stoica <andrei00stoica@gmail.com>
2021-03-01 23:37:07 +0200

    Add styling via node-sass

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './styles/global.scss';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
