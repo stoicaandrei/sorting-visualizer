f1c05121637fb7731bb8a646292340842af382f5
Andrei Stoica <andrei00stoica@gmail.com>
2021-03-02 23:01:40 +0200

    Add points argument to ArrayCanvas.tsx for coloring different bars based on the sorting state

--- src/views/Visualizer/ArrayCanvas.tsx
+++ src/views/Visualizer/ArrayCanvas.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
+import { SortingPoints } from './types';
+
 import Bar from './Bar';
 
 import ValueMapper from './ValueMapper';
 
 type Props = {
   array: number[];
+  points: SortingPoints;
 };
 
-const ArrayCanvas: React.FC<Props> = ({ array }) => {
+const ArrayCanvas: React.FC<Props> = ({ array, points }) => {
   const canvasHeight = 200;
   const canvasWidth = 400;
   const gapWidth = 20;
@@ -34,12 +37,12 @@ const ArrayCanvas: React.FC<Props> = ({ array }) => {
         width: canvasWidth,
       }}
     >
-      {array.map((nr) => (
+      {array.map((nr, index) => (
         <Bar
           key={nr}
           height={heightMapper(nr)}
           width={barWidth}
-          color={barColor}
+          color={points[index] || barColor}
         />
       ))}
     </div>
--- src/views/Visualizer/types.ts
+++ src/views/Visualizer/types.ts
@@ -1,11 +1,7 @@
-export type SortingState = {
-  array: number[];
-  points: {
-    position: number;
-    color?: string;
-  }[];
-};
+export type SortingState = { array: number[]; [key: string]: unknown };
 
 export type SortingFunction = (
   array: number[]
 ) => IterableIterator<SortingState>;
+
+export type SortingPoints = { [key: number]: string };
