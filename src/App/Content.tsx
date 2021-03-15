5bf409ca9199e865f42289933a9e6ded9bc2ddb4
Andrei Stoica <andrei00stoica@gmail.com>
2021-03-15 04:47:13 +0200

    Add the visualizer inside the Content.tsx

--- src/App/Content.tsx
+++ src/App/Content.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 import { Layout } from 'antd';
 
+import { Visualizer } from 'components';
+
 const Content: React.FC = () => {
-  return <Layout.Content>Visualizer</Layout.Content>;
+  return (
+    <Layout.Content>
+      <Visualizer />
+    </Layout.Content>
+  );
 };
 
 export default Content;
