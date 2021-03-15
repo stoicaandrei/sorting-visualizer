234be3fb8ca878b6b6378ac45c871c6a6af810bc
Andrei Stoica <andrei00stoica@gmail.com>
2021-03-15 04:49:12 +0200

    Add AlgorithmSelector and AlgorithmEditor inside Sider.tsx

--- src/App/Sider.tsx
+++ src/App/Sider.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 import { Layout } from 'antd';
 
+import { AlgorithmEditor, AlgorithmSelector } from 'components';
+
 const Sider: React.FC = () => {
   return (
-    <Layout.Sider
-      collapsible
-      collapsedWidth={0}
-      width="40%"
-    >
-      <span style={{ color: 'white' }}>Code editor</span>
+    <Layout.Sider collapsible collapsedWidth={0} width="40%">
+      <AlgorithmSelector />
+      <AlgorithmEditor />/
     </Layout.Sider>
   );
 };
