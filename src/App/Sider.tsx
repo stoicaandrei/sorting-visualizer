0ff24c3e8c9c7b8f577f9c71c6dd20a06969a81e
Andrei Stoica <andrei00stoica@gmail.com>
2021-03-15 04:45:39 +0200

    Create a template sider

--- src/App/Sider.tsx
+++ src/App/Sider.tsx
@@ -3,7 +3,15 @@ import React from 'react';
 import { Layout } from 'antd';
 
 const Sider: React.FC = () => {
-  return <Layout.Sider />;
+  return (
+    <Layout.Sider
+      collapsible
+      collapsedWidth={0}
+      width="40%"
+    >
+      <span style={{ color: 'white' }}>Code editor</span>
+    </Layout.Sider>
+  );
 };
 
 export default Sider;
