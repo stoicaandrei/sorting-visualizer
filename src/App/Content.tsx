e535d30e62fb526b77e595ba8a4d909e0babca9a
Andrei Stoica <andrei00stoica@gmail.com>
2021-03-15 04:46:25 +0200

    Create basic content

--- src/App/Content.tsx
+++ src/App/Content.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Layout } from 'antd';
 
 const Content: React.FC = () => {
-  return <Layout.Content />;
+  return <Layout.Content>Visualizer</Layout.Content>;
 };
 
 export default Content;
