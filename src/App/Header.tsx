619de42dad71075eef61fb841e7373e3e26cd659
Andrei Stoica <andrei00stoica@gmail.com>
2021-03-15 04:31:21 +0200

    Add mock buttons in header

--- src/App/Header.tsx
+++ src/App/Header.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
-import { Layout } from 'antd';
+import { Layout, Menu, Button } from 'antd';
 
 const Header: React.FC = () => {
-  return <Layout.Header />;
+  return (
+    <Layout.Header>
+      <Menu theme="dark" mode="horizontal" selectable={false}>
+        <Menu.Item>Play</Menu.Item>
+        <Menu.Item>Generate Array</Menu.Item>
+        <Menu.Item>Custom Array</Menu.Item>
+        <Menu.Item>Size</Menu.Item>
+        <Menu.Item>Speed</Menu.Item>
+      </Menu>
+    </Layout.Header>
+  );
 };
 
 export default Header;
