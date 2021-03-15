7a957c475a525095de6bab606e9f70b079e32bb5
Andrei Stoica <andrei00stoica@gmail.com>
2021-03-15 04:57:27 +0200

    Add play and generate array functionality to Header

--- src/App/Header/index.tsx
+++ src/App/Header/index.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 
 import { Layout, Menu } from 'antd';
 
+import { useArrayActions, useArrayState, useAlgorithmActions } from 'contexts';
+
 const Header: React.FC = () => {
+  const { arrayLength } = useArrayState();
+  const { generateArray } = useArrayActions();
+  const { compileAlgorithm } = useAlgorithmActions();
+
   return (
     <Layout.Header>
       <Menu theme="dark" mode="horizontal" selectable={false}>
-        <Menu.Item>Play</Menu.Item>
-        <Menu.Item>Generate Array</Menu.Item>
-        <Menu.Item>Custom Array</Menu.Item>
-        <Menu.Item>Size</Menu.Item>
-        <Menu.Item>Speed</Menu.Item>
+        <>
+          <Menu.Item onClick={compileAlgorithm}>Play</Menu.Item>
+          <Menu.Item onClick={() => generateArray(arrayLength)}>
+            Generate New Array
+          </Menu.Item>
+
+          <Menu.Item>Custom Array</Menu.Item>
+          <Menu.Item>Size</Menu.Item>
+          <Menu.Item>Speed</Menu.Item>
+        </>
       </Menu>
     </Layout.Header>
   );
