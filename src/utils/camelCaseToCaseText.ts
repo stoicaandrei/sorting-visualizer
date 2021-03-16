ae08fe3454129f5d857977edb4bd63cd6c1e3731
Andrei Stoica <andrei00stoica@gmail.com>
2021-03-16 04:26:48 +0200

    Create camelCaseToCaseText.ts util

---
+++ src/utils/camelCaseToCaseText.ts
@@ -0,0 +1,6 @@
+const camelCaseToCaseText = (s: string) => {
+  const result = s.replace(/([A-Z])/g, ' $1');
+  return result.charAt(0).toUpperCase() + result.slice(1);
+};
+
+export default camelCaseToCaseText;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export { default as shuffleArray } from './shuffleArray';
 export { default as ValueMapper } from './ValueMapper';
 export { default as generateArrayOfLength } from './generateArrayOfLength';
 export { default as colorMapToSortingPoints } from './colorMapToSortingPoints';
+export { default as camelCaseToCaseText } from './camelCaseToCaseText';
