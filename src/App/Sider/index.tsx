af8a45bda3f8ceaff38fc8347698b74a060900be
Andrei Stoica <andrei00stoica@gmail.com>
2021-03-19 22:43:16 +0200

    Make the sider responsive

--- src/App/Sider/index.tsx
+++ src/App/Sider/index.tsx
@@ -5,9 +5,18 @@ import { Layout } from 'antd';
 import AlgorithmSelector from './AlgorithmSelector';
 import AlgorithmEditor from './AlgorithmEditor';
 
+import { useWindowSize } from 'hooks';
+
 const Sider: React.FC = () => {
+  const [windowWidth] = useWindowSize();
+
+  const siderTriggerWidth = 36;
+  const smallWidth = windowWidth - siderTriggerWidth * 2;
+  const bigWidth = 400;
+  const siderWidth = Math.min(smallWidth, bigWidth);
+
   return (
-    <Layout.Sider collapsible collapsedWidth={0} width="400px">
+    <Layout.Sider collapsible collapsedWidth={0} width={siderWidth}>
       <AlgorithmSelector />
       <AlgorithmEditor />
     </Layout.Sider>
